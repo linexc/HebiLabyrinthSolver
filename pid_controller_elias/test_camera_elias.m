51d491ba9b82edbae6bf980f009ea3e015d24e9f
Elias Dejene <elias.dejene@tum.de>
2021-07-08 19:29:03 +0200

    Implement mapping from pid-output to theta for hebi.

--- pid_controller_elias/test_camera_elias.m
+++ pid_controller_elias/test_camera_elias.m
@@ -29,14 +29,14 @@ frameCount = 0;
 error_x = 0.0;
 error_old_x = 0.0;
 error_sum_x = 0.0;
-theta_1 = 0.0;
+theta_kurz = 0.0;
 error_y = 0.0;
 error_old_y = 0.0;
 error_sum_y = 0.0;
-theta_2 = 0.0;
+theta_lang = 0.0;
 
 % execute loop at fixed frequency
-freq = 100; %Hz
+freq = 1000; %Hz
 r = rateControl(freq);    % 10 Hz
 dt = 1/freq;    
 while true 
@@ -61,30 +61,52 @@ while true
     
     
     %% compute error
-    K_p_x = 1/100;
-    K_i_x = 0;
-    K_d_x = 0;
+    K_p_x = 1/8;%1/3;
+    K_i_x = 1/2;%1/2;
+    K_d_x = 100;
     error_old_x = error_x;
     error_x = board_center_pos(1) - ctr(1);
     error_sum_x = error_sum_x + K_i_x*dt*error_x;
-    theta_1 = K_p_x*error_x + error_sum_x + K_d_x*(error_old_x - error_x)/dt
-    
-    K_p_y = 1/1000;
-    K_i_y = 0;
-    K_d_y = 0;
+    theta_lang = K_p_x*error_x + error_sum_x + K_d_x*(error_old_x - error_x)/dt;
+    if (error_x < 1e-3)
+        error_sum_x = 0.0;
+    end
+        
+    K_p_y = 1/7;%1/3;
+    K_i_y = 1/2;%1/5;
+    K_d_y = 100;
     error_old_y = error_y;
-    error_y = -(board_center_pos(2) - ctr(2));
+    error_y = board_center_pos(2) - ctr(2);
     error_sum_y = error_sum_y + K_i_y*dt*error_y;
-    theta_2 = K_p_y*error_y + error_sum_y + K_d_y*(error_old_y - error_y)/dt
+    theta_kurz = K_p_y*error_y + error_sum_y + K_d_y*(error_old_y - error_y)/dt;
+    if (error_y < 1e-3)
+        error_sum_y = 0.0;
+    end
     
-    %% compute offset for servos
-    theta_1 = theta_1 + offset(1);
-    theta_2 = theta_2 + offset(2);
-            
+    %% map errors to thetas
+    theta_kurz_send = map_error_y(theta_kurz);
+%     theta_kurz_send = 0.2;
+    theta_lang_send = map_error_x(theta_lang);
+    
+    
+    %% debug
+    error = [error_x, error_y]
+    theta = [theta_lang, theta_kurz]
+    theta_send = [theta_lang_send, theta_kurz_send]
+
+    
     %% set actuators
-    cmd.position = [theta_1, theta_2];
+    cmd.position = [theta_kurz_send, theta_lang_send];
     group.send(cmd);
     
     waitfor(r);
 end
     
+
+function theta_l = map_error_x(e_x)
+    theta_l = 3.85 + 3.85/207*e_x;
+end
+
+function theta_k = map_error_y(e_y)
+    theta_k = 3.45 - 3.45/175*e_y;
+end
